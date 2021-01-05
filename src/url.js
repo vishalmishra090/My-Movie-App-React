27a6176051b6c021ba892d98cf43869b26c620bb
Vishal Mishra <vishalmishra090@gmail.com>
2021-01-06 04:21:24 +0530

    update secure image place holder

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
 export const pos_img_url = "https://image.tmdb.org/t/p/w342";
 export const ph_img_url =
-  "http://placehold.jp/d7d9db/003366/342x513.png?text=No%20Image%0A";
+  "https://dummyimage.com/342x513/d9d9d9/382a63.png&text=No+Image";
