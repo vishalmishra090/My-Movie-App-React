7c8b0c0d351c544a861845c7ac7ca0af90ae295f
Vishal Mishra <vishalmishra090@gmail.com>
2021-01-09 14:40:33 +0530

    infinite scroll update

--- src/lambda/handelSearch.js
+++ src/lambda/handelSearch.js
@@ -3,8 +3,8 @@ const axios = require("axios");
 const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}`
 
 exports.handler = async function (event, context) {
-    let value = event.queryStringParameters.query;
-    let respons = await axios.get(apiUrl + "&query=" + value)
+    let {query,page} = event.queryStringParameters;
+    let respons = await axios.get(apiUrl + "&query=" + query + "&page=" + page)
     let data = respons.data
   return {
     statusCode: 200,
